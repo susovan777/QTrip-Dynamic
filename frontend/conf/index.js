eab50139fe1f45cf20628c91188d023660991654
CrioUser <crio-user@criodo.com>
2023-09-25 01:04:33 +0530

    ME_QTRIPDYNAMIC_MODULE_CITIES

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 // instructed in Milestone 2 - 🚩 Activity 3
-const config = { backendEndpoint: "http://3.7.59.23:8082" };
+const config = { backendEndpoint: "http://65.2.136.235:8082" };
 
 export default config;
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -19,21 +19,23 @@ async function fetchCities() {
   // 1. Fetch cities using the Backend API and return the data
   let cityData;
   try {
-    let cityFromApi = await fetch("http://3.7.59.23:8082/cities/")
-    let jsonData = await cityFromApi.json()
+    let cityFromApi = await fetch("http://65.2.136.235:8082/cities/");
+    let jsonData = await cityFromApi
+      .json()
       // .then((jsonData) => jsonData.json())
-      .then((finalData) => { // instructed in Milestone 2 - 🚩 Activity 4
+      .then((finalData) => {
+        // instructed in Milestone 2 - 🚩 Activity 4
         let cityArr = finalData.map((cities) => cities);
         // console.log(cityArr);
         cityData = cityArr;
         return cityArr;
       });
 
-      // instructed in Milestone 2 - 🚩 Activity 7 (2nd last)
-      // checking if the fetching is successfull or not
-      if(!cityFromApi.ok) {
-        throw new Error("Network response was not OK!");
-      }
+    // instructed in Milestone 2 - 🚩 Activity 7 (2nd last)
+    // checking if the fetching is successfull or not
+    if (!cityFromApi.ok) {
+      throw new Error("Network response was not OK!");
+    }
   } catch (err) {
     // console.log(err);
     return null; // instructed in Milestone 2 - 🚩 Activity 8 (last)
@@ -45,6 +47,26 @@ async function fetchCities() {
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
+  let card_div = document.createElement("div");
+  card_div.setAttribute("class", "mb-4 col-sm-6 col-lg-3"); // same class for all cards
+
+  card_div.innerHTML = `
+  <a href="pages/adventures/?city=${id}" id="${id}">
+    <div class="tile">
+      <div class="tile-text d-flex flex-column align-items-center">
+        <h5>${city}</h5>
+        <p>${description}</p>
+      </div>
+      <img
+      src=${image}
+      class="img-fluid"
+      alt=${id}
+      />
+    </div>
+  </a>
+  `;
+
+  document.getElementById("data").append(card_div);
 }
 
 export { init, fetchCities, addCityToDOM };
